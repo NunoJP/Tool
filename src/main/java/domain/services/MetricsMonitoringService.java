8f323738c6dd8e19675c2a83cbc6f35c6371e739
NJP <40292@alunos.isel.ipl.pt>
2022-10-03 14:56:00 +0100

    2022-10-03: Changing monitoring update to polling

--- src/main/java/domain/services/MetricsMonitoringService.java
+++ src/main/java/domain/services/MetricsMonitoringService.java
@@ -2,11 +2,15 @@ package domain.services;
 
 import data.dataaccess.reader.DynamicLogFileReaderConsumer;
 import data.dataaccess.reader.LogFileReader;
+import data.dataaccess.reader.LogFileReaderWrapper;
 import domain.entities.domainobjects.MetricsProfile;
 import domain.entities.domainobjects.MetricsReport;
 import domain.entities.domainobjects.ParsingProfile;
 
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Consumer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class MetricsMonitoringService {
     private Consumer<MetricsReport> reportConsumer;
@@ -14,6 +18,11 @@ public class MetricsMonitoringService {
     private ParsingProfile parsingProfile;
     private MetricsProfile metricsProfile;
     private DynamicLogFileReaderConsumer logLineConsumer;
+    private MetricsReport previousReport;
+    private MetricsReport currentReport;
+    private boolean wasChanged = true;
+    private AtomicBoolean keepGoing = new AtomicBoolean(true);
+    private static final Logger LOGGER = Logger.getLogger(MetricsMonitoringService.class.getName());
 
     public MetricsMonitoringService(Consumer<MetricsReport> reportConsumer, LogFileReader logFileReader,
                                     ParsingProfile parsingProfile, MetricsProfile metricsProfile) {
@@ -24,11 +33,44 @@ public class MetricsMonitoringService {
     }
 
     public void start() {
-        logLineConsumer = new DynamicLogFileReaderConsumer(reportConsumer, parsingProfile, metricsProfile);
-        logFileReader.readDynamic((a) -> {}, logLineConsumer);
+        System.out.println("MetricsMonitoringService 1");
+        logLineConsumer = new DynamicLogFileReaderConsumer((this::updateLocalReport), parsingProfile, metricsProfile);
+        System.out.println("MetricsMonitoringService 2");
+        LogFileReaderWrapper logFileReaderWrapper = new LogFileReaderWrapper(logFileReader, logLineConsumer);
+        (new Thread(logFileReaderWrapper)).start();
+
+
+        System.out.println("MetricsMonitoringService 3");
+        while (keepGoing.get()) {
+            System.out.println("MetricsMonitoringService 4");
+            try {
+                Thread.sleep(300);
+            } catch (InterruptedException e) {
+                LOGGER.log(Level.SEVERE, "Error in thread sleep at MetricsMonitoringService", e);
+            }
+            System.out.println("MetricsMonitoringService 5");
+            if (wasChanged) {
+                System.out.println("changed");
+                reportConsumer.accept(getCurrentReportAndMarkUnchanged());
+            }
+            System.out.println("MetricsMonitoringService 6");
+        }
     }
 
     public void stop() {
+        keepGoing.set(false);
         logFileReader.stop();
+    }
+
+    private void updateLocalReport(MetricsReport report){
+//        System.out.println("MetricsMonitoringService 7");
+        this.previousReport = currentReport;
+        this.currentReport = report;
+        this.wasChanged = true;
+    }
+
+    private MetricsReport getCurrentReportAndMarkUnchanged() {
+        this.wasChanged = false;
+        return currentReport;
     }
 }
